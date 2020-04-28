2dc2ca637a14604081bff462e651fe6b5ef9bceb
zr67800 <17163263+zr67800@users.noreply.github.com>
2020-04-28 15:35:34 +0800

    initial UI file

---
+++ main_UI.py
@@ -0,0 +1,13 @@
+'''
+JUST MOVE -- an fitness game app prototype with camera based human posture recognition.
+
+
+This is the entry point of the game. To run the game, use Python 3.8+ with opencv. 
+    python main_UI.py
+
+This file includes the main part of the UI control and navigation.
+
+'''
+
+
+import tkinter
